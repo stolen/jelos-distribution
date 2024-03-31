fc371df5067fb14004533004de9c1ae000252d26
Danil Zagoskin <z@gosk.in>
2024-03-31 13:16:24 +0300

    libmali: use fresh upstream

--- packages/graphics/libmali/package.mk
+++ packages/graphics/libmali/package.mk
@@ -5,31 +5,47 @@
 PKG_NAME="libmali"
 PKG_ARCH="arm aarch64"
 PKG_LICENSE="nonfree"
-PKG_SITE="https://github.com/JustEnoughLinuxOS/libmali"
-PKG_VERSION="b9619b998cd9a019dacd6f5a4058c757ec0ed382"
+PKG_SITE="https://github.com/tsukumijima/libmali-rockchip"
+PKG_DEPENDS_TARGET="toolchain libdrm patchelf"
 
-if [ "${TARGET_ARCH}" = "aarch64" ]; then
-  INSTARCH="aarch64-linux-gnu"
-elif [ "${TARGET_ARCH}" = "arm" ]; then
-  INSTARCH="arm-linux-gnueabihf"
-fi
+PKG_VERSION="v1.9-1-b9619b9"
+# zip format makes extract very fast (<1s). tgz takes 20 seconds to scan the whole file
+PKG_URL="https://github.com/tsukumijima/libmali-rockchip/archive/refs/tags/${PKG_VERSION}.zip"
 
-PKG_URL="https://github.com/tsukumijima/libmali-rockchip/raw/${PKG_VERSION}/lib/${INSTARCH}/libmali-bifrost-${MALI_FAMILY}-${MALI_VERSION}-wayland-gbm.so"
-PKG_TOOLCHAIN="make"
-MALI_LIB_VERSION="1.9.0"
-PKG_DEPENDS_TARGET="toolchain libdrm"
-
-
-if [ "${VULKAN_SUPPORT}" = "yes" ]; then
-  PKG_DEPENDS_TARGET+=" vulkan-tools"
-fi
+PKG_TOOLCHAIN="meson"
 
 PKG_LONGDESC="OpenGL ES user-space binary for the ARM Mali GPU family"
 
+case "${DISPLAYSERVER}" in
+  wl)
+    PLATFORM="wayland-gbm"
+    PKG_DEPENDS_TARGET+=" wayland"
+    ;;
+  x11)
+    PLATFORM="x11-gbm"
+    ;;
+  *)
+    PLATFORM="gbm"
+    ;;
+esac
+
+PKG_BUILD_FLAGS="-strip"
+PKG_MESON_OPTS_TARGET+=" -Darch=${ARCH} -Dgpu=${MALI_FAMILY} -Dversion=${MALI_VERSION} -Dplatform=${PLATFORM} \
+					     -Dkhr-header=true -Dvendor-package=false -Dwrappers=enabled"
+
 
 unpack() {
-  declare -p | grep -F 'sources'
-  echo "${PKG_NAME}" "${PKG_UNPACK_DIR}"
   mkdir -p "${PKG_BUILD}"
-  cp "${SOURCES}/${PKG_NAME}/${PKG_SOURCE_NAME}" "${PKG_BUILD}/libmali.so.1"
+  cd "${PKG_BUILD}"
+  pwd
+  # Extract only what is needed
+  LIBNAME="libmali-${MALI_FAMILY}-${MALI_VERSION}-${PLATFORM}.so"
+  unzip -q "${SOURCES}/${PKG_NAME}/${PKG_SOURCE_NAME}" "*/hook/*" "*/include/*" "*/scripts/*" "*/meson*" "*/${LIBNAME}"
+  mv libmali-rockchip-*/* .
+  ln -s lib optimize_3
+}
+
+pre_makeinstall_target() {
+  readelf -Ws hook/libmali-hook.so | sed -nE 's|^.*GLOBAL DEFAULT *[0-9]+ *([a-z].*)$|\1 _\1|p' | sort -u > rename.syms
+  patchelf --rename-dynamic-symbols rename.syms --add-needed libmali-hook.so.1 libmali-prebuilt.so
 }
